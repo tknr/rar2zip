f6d25dd8409dd8563245da8fe00d82a6e377d2f6
tknr <info@tknr.com>
2017-06-07 14:37:16 +0900

    2017年  6月  7日 水曜日 14:37:16 JST

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# rezip
+# rar2zip
+
---
+++ rar2zip.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+export IFS=$'\n'
+#!/bin/bash
+export IFS=$'\n'
+DIR=$(cd $(dirname $0); pwd)
+cd $DIR
+
+DATE=`date +%Y_%m_%d`
+argv=("$@")
+CMDNAME=`basename $0`
+
+if [ $# -eq 0 ]; then
+	    echo "Usage : ${CMDNAME} [dirname]"
+	        exit 1
+fi
+
+for TARGET_DIR in ${argv}
+do
+	echo ${TARGET_DIR}
+	for FILENAME in `find "${TARGET_DIR}" -name "*.rar"`
+	do
+		echo ${FILENAME}
+	        folder=${FILENAME%%.rar}
+	        mkdir "${folder}" || continue
+	        unrar x "${FILENAME}" "${folder}" || continue
+	        zip -r "${folder}.zip" "${folder}" || continue
+	        rm -Rf "${folder}"
+	        rm -f "${FILENAME}"
+	done
+done
